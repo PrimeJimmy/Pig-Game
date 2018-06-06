dbc30d55c5af32eabe4944cac5d69a8dd672f421
James Lawson <39174577+PrimeJimmy@users.noreply.github.com>
2018-06-06 02:32:05 -0400

    changed winning score

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ document.querySelector('.btn-hold').addEventListener('click', function () {
 		document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer]
 
 		//3. Check if player won the game
-		if (scores[activePlayer] >= 20) {
+		if (scores[activePlayer] >= 100) {
 			document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
 			document.querySelector('.dice').style.display = 'none';
 			document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
@@ -98,38 +98,3 @@ function init() {
 
 	document.querySelector('.player-0-panel').classList.add('active');
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
